b7bf7f3134c724a448261c7ef386c8a7dbbd73eb
as1-design <asingla1@xtec.cat>
2025-08-15 20:16:37 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,25 @@
 
-const CACHE_NAME = 'vocab-trainer-v12.7';
+const CACHE_NAME = 'vocab-trainer-v12.8';
 const APP_SHELL = [
   './index.html',
   './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png'
 ];
-
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(self.skipWaiting())
   );
 });
-
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })))
     .then(self.clients.claim())
   );
 });
-
 self.addEventListener('fetch', (event) => {
   const req = event.request;
   if (req.method !== 'GET') return;
-
   event.respondWith((async () => {
     const cache = await caches.open(CACHE_NAME);
     const cached = await cache.match(req, { ignoreVary: true, ignoreSearch: false });
